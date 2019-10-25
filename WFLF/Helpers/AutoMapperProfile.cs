51ccd18ce6bab9d19e19f4aafc746810937a5300
Mike Walters <mike@beringbullet.com>
2019-10-25 13:04:30 -0500

    base API Authentication

---
+++ WFLF/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using WFLF.Entities;
+using WFLF.Models.Users;
+
+namespace WFLF.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserModel>();
+            CreateMap<RegisterModel, User>();
+            CreateMap<UpdateModel, User>();
+        }
+    }
+}
---
+++ WFLF/Helpers/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WFLF.Entities;
+
+namespace WFLF.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
